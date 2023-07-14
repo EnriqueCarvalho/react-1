47bf52a039ab4a54ae111eeae87c5b8086952f0c
EnriqueCarvalho <enriquecarvalho60@gmail.com>
2023-07-13 23:38:50 -0300

    adição dos navigators

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import react from "react";
-import { View,Text } from "react-native-web";
+import { View, Text } from "react-native";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from "./src/pages/home";
+import Produtos from "./src/pages/produtos";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App(){
-  return(
-    <View>
-      <Text>Meu primeiro projeto com React</Text>
-    </View>
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Produtos" component={Produtos} />
+      </Tab.Navigator>
+    </NavigationContainer>
+
   )
 }

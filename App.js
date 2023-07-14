c2f8e72334634d903c7d847179e6a35443da7ed4
EnriqueCarvalho <enriquecarvalho60@gmail.com>
2023-07-13 23:02:39 -0300

    primeiro commit de react-native

---
+++ App.js
@@ -0,0 +1,10 @@
+import react from "react";
+import { View,Text } from "react-native-web";
+
+export default function App(){
+  return(
+    <View>
+      <Text>Meu primeiro projeto com React</Text>
+    </View>
+  )
+}
